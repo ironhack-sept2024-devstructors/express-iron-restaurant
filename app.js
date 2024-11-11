bbec08c342b070fe88458a32e08e8da24fdae7f1
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-11-11 12:29:52 +0100

    final code from todays codealong

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const express = require("express");
+const logger = require("morgan");
+
+const pizzasArr = require("./data/pizzas");
 
 const app = express();
+
+const PORT = 3000;
+
+// Setup the request logger to run on each request
+app.use(logger("dev"));
+
+// Make the static files inside of the `public/` folder publicly accessible
+app.use(express.static("public"));
+
+// JSON middleware to parse incoming HTTP requests that contain JSON
+app.use(express.json());
+
 
 
 // app.get(path, code);
 // app.get(path, function(req, res, next){});
 
 
-app.get("/", function(req, res, next){    
-    console.log("we recieved a GET request for the HOME page...");
-    res.send("This is the homepage");
+// GET /
+app.get("/", function (req, res, next) {
+    res.sendFile(__dirname + "/views/home.html");
 })
 
 
-app.get("/contact", function(req, res, next){
-    console.log("we recieved a GET request for the CONTACT page...");
-    res.send("This is contact page");
+// GET /contact
+app.get("/contact", function (req, res, next) {
+    res.sendFile(__dirname + "/views/contact.html");
 });
 
 
-app.listen(3000, function(){ console.log("Server listening on port 3000...")});
+// GET /pizzas
+app.get("/pizzas", function (req, res, next) {
+    res.json(pizzasArr);
+});
+
+
+app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`) });
 
 
