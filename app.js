ea4d337ac5b12d62fc7f6bb6d48c329b089a3133
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-11-12 10:59:18 +0100

    refactor: simplify app.js

--- app.js
+++ app.js
@@ -18,43 +18,39 @@ app.use(express.json());
 
 
 
-// app.get(path, code);
-// app.get(path, function(req, res, next){});
-
-
 // GET /
-app.get("/", function (req, res, next) {
+app.get("/", (req, res, next) => {
     res.sendFile(__dirname + "/views/home.html");
 })
 
 
 // GET /contact
-app.get("/contact", function (req, res, next) {
+app.get("/contact", (req, res, next) => {
     res.sendFile(__dirname + "/views/contact.html");
 });
 
 
 // GET /pizzas
-app.get("/pizzas", function (req, res, next) {
+app.get("/pizzas", (req, res, next) => {
     res.json(pizzasArr);
 });
 
 
 // GET /pizzas/:pizzaId
-app.get("/pizzas/:pizzaId", function(req, res, next){
+app.get("/pizzas/:pizzaId", (req, res, next) => {
 
     const { pizzaId } = req.params;
 
     const pizzaDetails = pizzasArr.find((pizzaObj) => {
         return pizzaObj.id == pizzaId;
-    });    
-    
+    });
+
     res.json(pizzaDetails);
 });
 
 
 
 
-app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`) });
+app.listen(PORT, () => { console.log(`Server listening on port ${PORT}...`) });
 
 
